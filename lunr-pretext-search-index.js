73ae2f269258278f855e9904271bf93c9f2a6ba7
Jonny Comes <jcomes@collegeofidaho.edu>
2023-09-26 02:25:24 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,16 +7,16 @@ var ptx_lunr_docs = [
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
-  "body": "  My Website   copyright  "
+  "body": " copyright  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-set-notation",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-set-notation.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Sets: Conventions and Notation",
+  "body": " Sets: Conventions and Notation  We take a set to be a primitive notion. In other words, we will not attempt to give a formal definition of a set It turns out that formally defining a set is really difficult! . We will simply say that a set is a collection of elements . Given a set , we write to mean that is an element of . We write to means that is not an element of .  Some sets of numbers are denoted with special notation. For example, we write for the set of all real numbers For a formal definition of a real number, take a course on Real Analysis! . We write for the set of all natural numbers . In other words, in this class we define Thus and , but , and  Note that we are choosing not to include 0 as a natural number. This choice is not agreed upon by all mathematicians. .  An integer is a number that is either a natural number, the negative of a natural number, or zero. We denote the set of all integers by . In other words, Hence, , , , , but .  A rational number is a real number that can be written in the form with and . We write for the set of all rational numbers. For example, and . We also have and since and . It turns out that there are irrational numbers too: , , and . However, proving a number is irrational requires proof (see Example ???).  "
 },
 {
   "id": "colophon-2",
